9563b3671aa3301f7517796ddc6c7a100d320f4b
mars050504 <153522723+mars050504@users.noreply.github.com>
2025-06-03 10:57:24 +0700

    Initial commit

---
+++ frontend/src/hooks/useGetCompanyById.jsx
@@ -0,0 +1,28 @@
+import { setAllJobs } from '@/redux/jobSlice'
+import { JOB_API_END_POINT } from '@/utils/constant'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { COMPANY_API_END_POINT } from '../utils/constant'
+import { setSingleCompany } from '../redux/companySlice'
+
+const useGetCompanyById = (companyId) => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchSingleCompany = async () => {
+            try {
+                const res = await axios.get(`${COMPANY_API_END_POINT}/get/${companyId}`, { withCredentials: true });
+                if (res.data.success) {
+                    dispatch(setSingleCompany(res.data.company));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchSingleCompany();
+    }, [companyId,dispatch])
+}
+
+export default useGetCompanyById
